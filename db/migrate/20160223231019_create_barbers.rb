d08d662eb26108c096655c8784ece88b72aa66cb
Shannon <SNH@Shannons-MacBook-Air.local>
2016-02-23 18:14:35 -0500

    created model and controller for barbers

---
+++ db/migrate/20160223231019_create_barbers.rb
@@ -0,0 +1,12 @@
+class CreateBarbers < ActiveRecord::Migration
+  def change
+    create_table :barbers do |t|
+      t.string :name
+      t.string :instagram
+      t.integer :phone
+      t.string :photo
+
+      t.timestamps null: false
+    end
+  end
+end
