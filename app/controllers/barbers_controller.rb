8d46d76e892d8a38f59ce4767061075d5e087651
Shannon <SNH@Shannons-MacBook-Air.local>
2016-02-23 19:00:39 -0500

    add view and controller action

--- app/controllers/barbers_controller.rb
+++ app/controllers/barbers_controller.rb
@@ -1,2 +1,9 @@
 class BarbersController < ApplicationController
+ def index
+   @barbers = Barber.all
+ end
+
+ def show
+   @show = Barber.all
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'barbers#index'
+  resource :barbers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
