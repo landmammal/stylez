e1ea3b572906852bda760df090d0c37480182cb7
Roger Rodriguez <evolpirate@gmail.com>
2016-02-29 19:14:58 -0500

    add it customer table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229003821) do
+ActiveRecord::Schema.define(version: 20160301001402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160229003821) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer  "shop_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shops", force: :cascade do |t|
