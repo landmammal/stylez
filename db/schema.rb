e455439e1c6c816877fc418ddfcdc0e25c995a07
Gaztambide <francogaztambide@gmail.com>
2016-03-01 16:32:55 -0500

    remade users table for bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229003821) do
+ActiveRecord::Schema.define(version: 20160301213118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.string   "date"
+    t.string   "hour"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "barbers", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +37,17 @@ ActiveRecord::Schema.define(version: 20160229003821) do
     t.integer  "shop_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
@@ -37,6 +55,14 @@ ActiveRecord::Schema.define(version: 20160229003821) do
     t.string   "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
