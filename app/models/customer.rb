fc12388be627f6c5572553ddacb3717826ea959e
Roger Rodriguez <evolpirate@gmail.com>
2016-03-08 01:35:11 -0500

    removed raty rate ??

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,7 +10,7 @@ class Customer < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  ratyrate_rater
+  # ratyrate_rater
 
   def feed
 
